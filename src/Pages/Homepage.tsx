69d8934fc0234d4f4a33239f1e6b41df52b73bb8
Hamza Zaheer <malikhamza1619619@gmail.com>
2023-07-21 01:48:24 +0500

    shop categories section added

--- src/Pages/Homepage.tsx
+++ src/Pages/Homepage.tsx
@@ -1,10 +1,12 @@
 import Footer from "../components/Footer";
 import HeroSection from "../components/HeroSection";
+import ShopCategory from "../components/homepage/ShopCategory";
 
 const Homepage = () => {
   return (
     <div>
       <HeroSection />
+      <ShopCategory />
       <Footer />
     </div>
   );
---
+++ src/components/homepage/ShopCategory.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+
+const ShopCategory = () => {
+  return (
+    <div className="my-20 max-w-[1360px] mx-auto px-10">
+      <h2 className="text-text-color text-3xl">Shop Our Top Categories</h2>
+      <div className="flex justify-between">
+        <div className="w-[194px] h-[250px] rounded-xl relative overflow-hidden cursor-pointer">
+          <img
+            src="/assets/cat1Badminton.jpg"
+            alt="category 1 image"
+            className="rounded-xl w-full h-full hover:scale-110 ease-in-out duration-300"
+          />
+          <h4 className="absolute top-[10px] text-white left-2/4 -translate-x-2/4 font-bold tracking-widest text-2xl">
+            Badmiton
+          </h4>
+        </div>
+        <div className="w-[194px] h-[250px] rounded-xl relative overflow-hidden cursor-pointer">
+          <img
+            src="/assets/cat1Badminton.jpg"
+            alt="category 1 image"
+            className="rounded-xl w-full h-full hover:scale-110 ease-in-out duration-300"
+          />
+          <h4 className="absolute top-[10px] text-white left-2/4 -translate-x-2/4 font-bold tracking-widest text-2xl">
+            Badmiton
+          </h4>
+        </div>
+        <div className="w-[194px] h-[250px] rounded-xl relative overflow-hidden cursor-pointer">
+          <img
+            src="/assets/cat1Badminton.jpg"
+            alt="category 1 image"
+            className="rounded-xl w-full h-full hover:scale-110 ease-in-out duration-300"
+          />
+          <h4 className="absolute top-[10px] text-white left-2/4 -translate-x-2/4 font-bold tracking-widest text-2xl">
+            Badmiton
+          </h4>
+        </div>
+        <div className="w-[194px] h-[250px] rounded-xl relative overflow-hidden cursor-pointer">
+          <img
+            src="/assets/cat1Badminton.jpg"
+            alt="category 1 image"
+            className="rounded-xl w-full h-full hover:scale-110 ease-in-out duration-300"
+          />
+          <h4 className="absolute top-[10px] text-white left-2/4 -translate-x-2/4 font-bold tracking-widest text-2xl">
+            Badmiton
+          </h4>
+        </div>
+        <div className="w-[194px] h-[250px] rounded-xl relative overflow-hidden cursor-pointer">
+          <img
+            src="/assets/cat1Badminton.jpg"
+            alt="category 1 image"
+            className="rounded-xl w-full h-full hover:scale-110 ease-in-out duration-300"
+          />
+          <h4 className="absolute top-[10px] text-white left-2/4 -translate-x-2/4 font-bold tracking-widest text-2xl">
+            Badmiton
+          </h4>
+        </div>
+        <div className="w-[194px] h-[250px] rounded-xl relative overflow-hidden cursor-pointer">
+          <img
+            src="/assets/cat1Badminton.jpg"
+            alt="category 1 image"
+            className="rounded-xl w-full h-full hover:scale-110 ease-in-out duration-300"
+          />
+          <h4 className="absolute top-[10px] text-white left-2/4 -translate-x-2/4 font-bold tracking-widest text-2xl">
+            Badmiton
+          </h4>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ShopCategory;
